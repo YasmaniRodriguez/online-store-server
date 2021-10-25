abd43615299828b5060dbdaf4772b271c29fc937
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2021-10-24 21:28:01 -0300

    ajustes varios

--- server/middlewares/auth/passport-local-strategy.js
+++ server/middlewares/auth/passport-local-strategy.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
-const User = require("../db/MongoDB/models/users.js");
-const { compareHash } = require("../functions.js");
+const User = require("../../db/MongoDB/models/users.js");
+const { compareHash } = require("../../functions.js");
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
