5e3f0fe245b87434aa4024e0636ad6535fb973ed
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2021-12-12 16:05:25 -0300

    ajustes varios

--- tests/products.test.js
+++ tests/products.test.js
@@ -2,27 +2,25 @@ const baseUrl = "http://localhost:8080";
 const request = require("supertest")(baseUrl);
 const expect = require("chai").expect;
 const { buildProduct } = require("../utils/function");
+
 const API_PRODUCTS = "/products";
 const validators = [];
 
-const setValidator = (suite, key, value) => {
-	return validators.push({ suite, key, value });
+const setValidator = (key, value) => {
+	return validators.push({ key, value });
 };
 
-const getValidator = (suite, key) => {
-	const val = validators.filter(
-		(obj) => obj.suite === suite && obj.key === key
-	);
+const getValidator = (key) => {
+	const val = validators.filter((obj) => obj.key === key);
 	return val[0].value;
 };
+console.log("all ready to run tests of the suite products");
 
 describe("[unit tests for products endpoint]", () => {
-	let suite = "products";
-
 	describe("POST", () => {
 		it("should return state 201 and the created product", async () => {
 			let product = await buildProduct();
-			setValidator(suite, "code", product.code);
+			setValidator("code", product.code);
 			let response = await request.post(API_PRODUCTS).send(product);
 			expect(response.status).to.eql(201);
 			const data = response.body;
@@ -33,7 +31,7 @@ describe("[unit tests for products endpoint]", () => {
 
 	describe("GET", () => {
 		it("should return status 200 and the requested product", async () => {
-			let product = await getValidator(suite, "code");
+			let product = await getValidator("code");
 			let response = await request.get(`${API_PRODUCTS}?code=${product}`);
 			expect(response.status).to.eql(200);
 			const data = response.body;
@@ -45,7 +43,7 @@ describe("[unit tests for products endpoint]", () => {
 	describe("PUT", () => {
 		it("should return status 200 and the requested product with updated fields", async () => {
 			let fields = { stock: 5000 };
-			let product = await getValidator(suite, "code");
+			let product = await getValidator("code");
 			let response = await request
 				.put(`${API_PRODUCTS}/${product}`)
 				.send(fields);
@@ -59,7 +57,7 @@ describe("[unit tests for products endpoint]", () => {
 
 	describe("DELETE", () => {
 		it("should return status 200 and the deleted product", async () => {
-			let product = await getValidator(suite, "code");
+			let product = await getValidator("code");
 			let response = await request.delete(`${API_PRODUCTS}/${product}`);
 			expect(response.status).to.eql(200);
 			const data = response.body;
