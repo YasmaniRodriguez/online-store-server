9eccd4c195ffc02684ddff40d088510f71a658b8
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2022-01-20 01:02:59 -0300

    ajustes varios

--- src/public/main.js
+++ src/public/main.js
@@ -1,22 +1,25 @@
 //elements:
 const snackbar = $("#snackbar");
 const snackbarText = $("#snackbar > p");
-const signinBtn = $("#signin-submit-button");
-const signupBtn = $("#signup-submit-button");
-const username = $("#username").val();
-const password = $("#password").val();
+const signinButton = $("#signin-submit-button");
+const signupButton = $("#signup-submit-button");
+const usernameTextField = $("#username");
+const passwordTextField = $("#password");
 //
-signinBtn.click(function (e) {
+signinButton.click(function (e) {
 	e.preventDefault();
+	let email = usernameTextField.val();
+	let password = passwordTextField.val();
 
 	const request = $.ajax({
 		method: "POST",
 		url: "http://localhost:8080/api/login",
-		data: { email: username, password: password },
+		data: { email: email, password: password },
 	});
 
-	request.done(function (msg) {
-		console.log(msg);
+	request.done(function (response) {
+		let token = response.user.token;
+		sessionStorage.setItem("token", JSON.stringify(token));
 	});
 
 	request.fail(function (jqXHR, textStatus) {
