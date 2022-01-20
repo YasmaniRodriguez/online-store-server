595d6d6a748581c5e0d608b689fc07b19562f964
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2022-01-20 00:14:41 -0300

    ajustes varios

--- src/public/main.js
+++ src/public/main.js
@@ -1,13 +1,30 @@
+//elements:
 const snackbar = $("#snackbar");
 const snackbarText = $("#snackbar > p");
 const signinBtn = $("#signin-submit-button");
 const signupBtn = $("#signup-submit-button");
-
+const username = $("#username").val();
+const password = $("#password").val();
+//
 signinBtn.click(function (e) {
 	e.preventDefault();
-	showSnackBar("esto se va a descrontrolaaaaaaaaar");
+
+	const request = $.ajax({
+		method: "POST",
+		url: "http://localhost:8080/api/login",
+		data: { email: username, password: password },
+	});
+
+	request.done(function (msg) {
+		console.log(msg);
+	});
+
+	request.fail(function (jqXHR, textStatus) {
+		showSnackBar(jqXHR.responseJSON.message);
+	});
 });
 
+//snackbar:
 function showSnackBar(message) {
 	snackbarText.empty();
 	snackbarText.append(message);
