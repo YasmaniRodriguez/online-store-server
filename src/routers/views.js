4bf4e76df9233c03f6b1df29e09968ae285dc959
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2022-01-21 15:05:53 -0300

    ajustes varios

--- src/routers/views.js
+++ src/routers/views.js
@@ -2,19 +2,68 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	req.isAuthenticated() ? res.redirect("home") : res.redirect("signin");
+	console.log(req.user);
+	req.isAuthenticated()
+		? res.render("home", {
+				name: req.user.name,
+				lastname: req.user.lastname,
+				avatar: req.user.avatar,
+				birthday: req.user.birthday,
+				phone: req.user.phone,
+				email: req.user.email,
+				address: req.user.address,
+				role: req.user.role,
+				cart: req.user.cart,
+		  })
+		: res.redirect("signin");
 });
 
 router.get("/home", (req, res) => {
-	req.isAuthenticated() ? res.render("home", {}) : res.redirect("signin");
+	req.isAuthenticated()
+		? res.render("home", {
+				name: req.user.name,
+				lastname: req.user.lastname,
+				avatar: req.user.avatar,
+				birthday: req.user.birthday,
+				phone: req.user.phone,
+				email: req.user.email,
+				address: req.user.address,
+				role: req.user.role,
+				cart: req.user.cart,
+		  })
+		: res.redirect("signin");
 });
 
 router.get("/signin", (req, res) => {
-	req.isAuthenticated() ? res.render("home", {}) : res.render("signin");
+	req.isAuthenticated()
+		? res.render("home", {
+				name: req.user.name,
+				lastname: req.user.lastname,
+				avatar: req.user.avatar,
+				birthday: req.user.birthday,
+				phone: req.user.phone,
+				email: req.user.email,
+				address: req.user.address,
+				role: req.user.role,
+				cart: req.user.cart,
+		  })
+		: res.render("signin");
 });
 
 router.get("/signup", (req, res) => {
-	req.isAuthenticated() ? res.render("home", {}) : res.render("signup", {});
+	req.isAuthenticated()
+		? res.render("home", {
+				name: req.user.name,
+				lastname: req.user.lastname,
+				avatar: req.user.avatar,
+				birthday: req.user.birthday,
+				phone: req.user.phone,
+				email: req.user.email,
+				address: req.user.address,
+				role: req.user.role,
+				cart: req.user.cart,
+		  })
+		: res.render("signup", {});
 });
 
 module.exports = router;
