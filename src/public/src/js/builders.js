bcb6e620ded108a329f5e128f8a023aab43e1356
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2022-01-21 14:35:38 -0300

    ajustes varios

--- src/public/src/js/builders.js
+++ src/public/src/js/builders.js
@@ -3,7 +3,7 @@ function buildHtmlAccountMenu() {
     <div id="account-pop-up-menu">
         <h3>WHAT DO YOU WANT TO DO?</h3>
         <div id="signout-submit-button">
-            <p>Sign Out</p>
+            Sign Out
         </div>
     </div>
     `;
