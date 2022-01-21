06f99347fc719bc3d7fd114c612daa0705fcd620
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2022-01-21 15:05:53 -0300

    a

--- src/routers/views.js
+++ src/routers/views.js
@@ -2,19 +2,43 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	req.isAuthenticated() ? res.redirect("home") : res.redirect("signin");
+	req.isAuthenticated()
+		? res.render("home", {
+				name: req.user.name,
+				lastname: req.user.lastname,
+				avatar: req.user.avatar,
+		  })
+		: res.redirect("signin");
 });
 
 router.get("/home", (req, res) => {
-	req.isAuthenticated() ? res.render("home", {}) : res.redirect("signin");
+	req.isAuthenticated()
+		? res.render("home", {
+				name: req.user.name,
+				lastname: req.user.lastname,
+				avatar: req.user.avatar,
+		  })
+		: res.redirect("signin");
 });
 
 router.get("/signin", (req, res) => {
-	req.isAuthenticated() ? res.render("home", {}) : res.render("signin");
+	req.isAuthenticated()
+		? res.render("home", {
+				name: req.user.name,
+				lastname: req.user.lastname,
+				avatar: req.user.avatar,
+		  })
+		: res.render("signin");
 });
 
 router.get("/signup", (req, res) => {
-	req.isAuthenticated() ? res.render("home", {}) : res.render("signup", {});
+	req.isAuthenticated()
+		? res.render("home", {
+				name: req.user.name,
+				lastname: req.user.lastname,
+				avatar: req.user.avatar,
+		  })
+		: res.render("signup", {});
 });
 
 module.exports = router;
