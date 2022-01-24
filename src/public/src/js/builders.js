dbdcb6e71577dee0146da4554cfa1fa9ebc1663d
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2022-01-23 23:22:01 -0300

    ajustes varios

--- src/public/src/js/builders.js
+++ src/public/src/js/builders.js
@@ -2,12 +2,15 @@ function buildHtmlMessages(message) {
 	return `
     <li class="message-item">
         <div>
-            <img 
-                src="http://localhost:8080/src/${message.author.avatar}"
-                alt=""
-            />
-            <p>${message.author.email}</p>
-            <p>${message.updatedAt}</p>
+            <div>
+                <img 
+                    src="http://localhost:8080/src/${message.author.avatar}"
+                    alt=""
+                />
+                <p>${message.author.email}</p>
+                <p>${message.updatedAt}</p>
+            </div>
+            <span class="material-icons-outlined">more_vert</span>
         </div>
         <p>${message.message}</p>
     </li>
